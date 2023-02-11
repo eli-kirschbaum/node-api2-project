61d62a1213c930625506d0cdc05037f2af318745
Eli Kirschbaum <elikirschbaum@Elis-MacBook-Pro.local>
2023-02-11 16:04:15 -0600

    set up basic server functions and linked post requests to posts router

--- api/server.js
+++ api/server.js
@@ -1,2 +1,27 @@
 // implement your server here
 // require your posts router and connect it here
+
+//step 1
+const express = require('express');
+//step 2
+const postsRouter = require('./posts/posts-router');
+//step 1
+const server = express();
+//step 1
+server.use(express.json());
+
+//step 2
+//captures all requests made with /post
+//funnels them through this router
+//helps break up app into smaller components
+server.use('/api/posts', postsRouter)
+
+
+//step 1
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+});
+//step 1
+module.exports = server;
