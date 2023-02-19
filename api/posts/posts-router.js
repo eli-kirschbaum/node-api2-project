097383b8638eba4ed7fc37843cf18df4205309ce
Eli Kirschbaum <elikirschbaum@Elis-MacBook-Pro.local>
2023-02-19 15:48:19 -0600

    produced working code for first two get routers

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,6 +1,68 @@
 // implement your posts router here
 const express = require('express');
-
+const Post = require('./posts-model');
 const router = express.Router();
+
+//Returns an array of all the post objects 
+//contained in the database                                       
+router.get('/', (req, res) => {
+    Post.find()
+        .then(found => {
+            res.json(found)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The posts information could not be retrieved",
+                err: err.message,
+                stack: err.stack
+            })
+        })
+});
+
+//Returns the post object with the specified id
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist',
+            })
+        } else {
+            res.json(post)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The post with the specified ID does not exist",
+            err: err.message,
+            stack: err.stack,
+        })
+    }
+});
+
+//Creates a post 
+//using the information sent inside the request body 
+//and returns the newly created post object
+router.post('/', (req, res) => {
+    
+});
+
+//Removes the post with the specified id 
+//and returns the deleted post object
+router.delete('/:id', (req, res) => {
+    
+});
+
+//Updates the post with the specified id 
+//using data from the request body 
+//and returns the modified document, not the original
+router.put('/:id', (req, res) => {
+    
+});
+
+//Returns an array of all the comment objects 
+//associated with the post with the specified id
+router.get('/:id/messages', (req, res) => {
+    
+});
 
 module.exports = router;
