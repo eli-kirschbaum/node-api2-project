5df7c12ffaf69e6decb97e96e4386dc25c00e4a7
Eli Kirschbaum <elikirschbaum@Elis-MacBook-Pro.local>
2023-02-20 15:23:32 -0600

    put router is working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -68,16 +68,67 @@ router.post('/', (req, res) => {
 
 //Removes the post with the specified id 
 //and returns the deleted post object
-router.delete('/:id', (req, res) => {
-    
+router.delete('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist',
+            })
+        } else {
+            await Post.remove(req.params.id)
+            res.json(post)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The post could not be removed",
+            err: err.message,
+            stack: err.stack,
+        })
+    } 
 });
 
 //Updates the post with the specified id 
 //using data from the request body 
 //and returns the modified document, not the original
 router.put('/:id', (req, res) => {
-    
+    const { title, contents } = req.body;
+    if (!title || !contents) {
+        res.status(400).json({
+            message: 'Please provide title and contents for the post'
+        })
+    } else {
+        Post.findByIds(req.params.id)
+            .then(stuff => {
+                if (!stuff) {
+                    res.status(404).json({
+                        message: 'The post with the specified ID does not exist',
+                    })
+                } else {
+                    return Post.update(req.params.id, req.body)
+                }
+            })
+            .then(data => {
+                if (data) {
+                    return Post.findById(req.params.id)
+                }
+            })
+            .then(post => {
+                if (post) {
+                    res.json(post)
+                }
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "The posts information could not be retrieved",
+                    err: err.message,
+                    stack: err.stack,
+                })
+            })
+    }
 });
+   
+
 
 //Returns an array of all the comment objects 
 //associated with the post with the specified id
